22e8cb220794f04d9dd768fac00ba37b76b4cb45
Aline Grance <grance.aline@gmail.com>
2022-07-13 15:56:54 -0300

    Add requisito 3, inicio

---
+++ script.js
@@ -0,0 +1,13 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submitLogin = document.querySelector("button");
+
+submitLogin.addEventListener("click", (e) => {
+  console.log(email.value, password.value);
+  e.preventDefault();
+  if (email.value === "tryber@teste.com" && password.value === "123456") {
+    alert("Olá, tryber!");
+  } else {
+    alert("Email ou senha inválidos");
+  }
+});
