069070d1661da80ba7b302662323ac65aeb03530
Aline Grance <grance.aline@gmail.com>
2022-07-13 16:06:34 -0300

    Fix eslint até req3

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const submitLogin = document.querySelector("button");
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const submitLogin = document.querySelector('button');
 
-submitLogin.addEventListener("click", (e) => {
+submitLogin.addEventListener('click', (e) => {
   console.log(email.value, password.value);
   e.preventDefault();
-  if (email.value === "tryber@teste.com" && password.value === "123456") {
-    alert("Olá, Tryber!");
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 });
